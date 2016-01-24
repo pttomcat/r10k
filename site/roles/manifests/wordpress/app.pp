ecdceed92d2118dcefbbb6a2884af21d974129e9
tomcat <pt.tomcat@gmail.com>
2016-01-24 14:46:20 +0000

    finally

--- site/roles/manifests/wordpress/app.pp
+++ site/roles/manifests/wordpress/app.pp
@@ -3,9 +3,10 @@ class roles::wordpress::app  {
      class  { '::wordpress':
          wp_owner    =>  'wordpress',
          wp_group    =>  'wordpress',
-         db_user     =>  'wordpress',
+         db_user     =>  'wordpress1',
+         db_name     => 'wordpress1'
          db_password =>  'password',
-         install_url =>  'wordpress.home',
+         #         install_url =>  '',
          install_dir =>  '/var/www/wordpress',
    }
 
