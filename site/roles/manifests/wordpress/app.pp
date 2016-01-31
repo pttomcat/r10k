662c6e75ce275db56e610a99579733f18b9959e2
tomcat <pt.tomcat@gmail.com>
2016-01-31 00:57:16 +0000

    hiera

--- site/roles/manifests/wordpress/app.pp
+++ site/roles/manifests/wordpress/app.pp
@@ -1,12 +1,12 @@
 class roles::wordpress::app  {
-
+  $install_dir = hiera('roles::wordpress::app::install_dir')
      class  { '::wordpress':
          wp_owner    =>  'wordpress',
          wp_group    =>  'wordpress',
          db_user     =>  'wordpress1',
          #         db_name     =>  'wordpress1',
          #         db_password =>  'password',
-         install_dir =>  '/var/www/wordpress',
+         install_dir =>  $install_dir,
    }
 
 
